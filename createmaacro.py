9bee0ff236b1b8a0521c51f1a8dbda9a86983871
Arief Chaerudin <45324752+arief0408@users.noreply.github.com>
2025-02-04 03:34:27 +0700

    genesis

--- createmaacro.py
+++ createmaacro.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 
 # Load the Excel file into a DataFrame
-input_file = 'data_pruhajj.xlsx'  # Replace with your Excel file name
+input_file = 'data_cstg_prem_.xlsx'  # Replace with your Excel file name
 df = pd.read_excel(input_file)
 
 # Read the .txt file containing the template
-with open('mac_template_pruhajj.txt', 'r', encoding='utf-8') as template_file:
+with open('mac_template_cstg_prem_.txt', 'r', encoding='utf-8') as template_file:
     mac_template = template_file.read()
 
 # Replace NaN values in the DataFrame with empty strings
@@ -16,12 +16,29 @@ df = pd.read_excel(input_file).replace(np.nan, '', regex=True)
 # Generate .mac files for each row in the DataFrame
 for index, row in df.iterrows():
     # Generate the full path for the .mac file
-    formatted_agent = str(row.get('Agent', '')).zfill(8)  # Ensure it's 8 digits
-    formatted_partner = str(row.get('Partner', '')).zfill(8)  # Ensure it's 8 digits
-    formatted_mandate = str(row.get('Mandate', '')).zfill(5)  # Ensure it's 8 digits
+    formatted_Policy_1 = str(row.get('Policy_1', '')).zfill(8)
+    formatted_Premium_1 = str(row.get('Premium_1', '')).zfill(8)
+    formatted_Policy_2 = str(row.get('Policy_2', '')).zfill(8)
+    formatted_Premium_2 = str(row.get('Premium_2', '')).zfill(8)
+    formatted_Policy_3 = str(row.get('Policy_3', '')).zfill(8)
+    formatted_Premium_3 = str(row.get('Premium_3', '')).zfill(8)
+    formatted_Policy_4 = str(row.get('Policy_4', '')).zfill(8)
+    formatted_Premium_4 = str(row.get('Premium_4', '')).zfill(8)
+    formatted_Policy_5 = str(row.get('Policy_5', '')).zfill(8)
+    formatted_Premium_5 = str(row.get('Premium_5', '')).zfill(8)
+    formatted_Policy_6 = str(row.get('Policy_6', '')).zfill(8)
+    formatted_Premium_6 = str(row.get('Premium_6', '')).zfill(8)
+    formatted_Policy_7 = str(row.get('Policy_7', '')).zfill(8)
+    formatted_Premium_7 = str(row.get('Premium_7', '')).zfill(8)
+    formatted_Policy_8 = str(row.get('Policy_8', '')).zfill(8)
+    formatted_Premium_8 = str(row.get('Premium_8', '')).zfill(8)
+    formatted_Policy_9 = str(row.get('Policy_9', '')).zfill(8)
+    formatted_Premium_9 = str(row.get('Premium_9', '')).zfill(8)
+    formatted_Policy_10 = str(row.get('Policy_10', '')).zfill(8)
+    formatted_Premium_10 = str(row.get('Premium_10', '')).zfill(8)
 
     # Get the IBM_PATH from the row
-    ibm_path = row.get('IBM_Path', '')  # Example: 'C:\\Fileshare_GIT_Pru\\CreationMacro'
+    ibm_path = row.get('IBM_Path', '')
 
     # Ensure the IBM_PATH exists and is valid
     if not os.path.exists(ibm_path):
@@ -30,36 +47,38 @@ for index, row in df.iterrows():
 
     # Format the content by replacing placeholders with values from the DataFrame
     formatted_content = mac_template.format(
-        Surname=row.get('Surname', ''),
-        Gender=row.get('Gender', ''),
-        Mortality=row.get('Mortality', ''),
-        DOB=row.get('DOB', ''),
-        Agent=formatted_agent,  # Add the formatted agent number here
-        Partner=formatted_partner,  # Add the formatted agent number here
-        Mandate=formatted_mandate,  # Add the formatted agent number here
-        Referral=row.get('Referral', ''),
-        Branch=row.get('Branch', ''),
-        Product=row.get('Product', ''),
-        Payment_Method=row.get('Payment_Method', ''),
-        Billing_Frequency=f"{row.get('Billing_Frequency', ''):02}",  # Ensures two digits
-        Height=row.get('Height', ''),
-        Weight=row.get('Weight', ''),
-        Insurable_Interest=row.get('Insurable_Interest', ''),
-        Cover_Type=row.get('Cover_Type', ''),
-        Risk_Cess_Term=row.get('Risk_Cess_Term', ''),
-        Prem_Cess_Term=row.get('Prem_Cess_Term', ''),
-        Total_Premium=row.get('Total_Premium', ''),
-        EPOL=row.get('EPOL', ''),
-        ESUB=row.get('ESUB', ''),
-        MAILFLAG=row.get('MAILFLAG', ''),
-        IBM_Path=ibm_path,  # Set IBM_Path to the full path of the .mac file
+        Bank_Code=row.get('Bank_Code', ''),
+        Action=row.get('Action', ''),
+        IBM_Path=ibm_path,
+        Policy_1=formatted_Policy_1,
+        Premium_1=formatted_Premium_1,
+        Policy_2=formatted_Policy_2,
+        Premium_2=formatted_Premium_2,
+        Policy_3=formatted_Policy_3,
+        Premium_3=formatted_Premium_3,
+        Policy_4=formatted_Policy_4,
+        Premium_4=formatted_Premium_4,
+        Policy_5=formatted_Policy_5,
+        Premium_5=formatted_Premium_5,
+        Policy_6=formatted_Policy_6,
+        Premium_6=formatted_Premium_6,
+        Policy_7=formatted_Policy_7,
+        Premium_7=formatted_Premium_7,
+        Policy_8=formatted_Policy_8,
+        Premium_8=formatted_Premium_8,
+        Policy_9=formatted_Policy_9,
+        Premium_9=formatted_Premium_9,
+        Policy_10=formatted_Policy_10,
+        Premium_10=formatted_Premium_10,
+        
+          # Set IBM_Path to the full path of the .mac file
 
         TC_Next=index + 2,
         cell=index + 2
     )
 
     # Create the file path for the .mac file
-    mac_filename = f'pruhajj_{index + 1}.mac'  # The file name (e.g., bospo_1.mac)
+    mac_filename = f'cstg_prem_{index + 1}.mac'  # The file name (e.g., bospo_1.mac)
     mac_file_path = os.path.join(ibm_path, mac_filename)  # Combine IBM_PATH with the filename
 
     # Save the formatted content to the .mac file in the specified IBM_PATH
