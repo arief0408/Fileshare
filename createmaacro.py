9a906c9fbfc56df8dc5c0dc410b1a344d44b90f3
Arief Chaerudin <45324752+arief0408@users.noreply.github.com>
2025-02-25 08:32:54 +0700

    Test

--- createmaacro.py
+++ createmaacro.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 
 # Load the Excel file into a DataFrame
-input_file = 'data_l1j.xlsx'  # Replace with your Excel file name
+input_file = 'data_PWMS.xlsx'  # Replace with your Excel file name
 df = pd.read_excel(input_file)
 
 # Read the .txt file containing the template
-with open('mac_template_l1j.txt', 'r', encoding='utf-8') as template_file:
+with open('mac_template_PWMS.txt', 'r', encoding='utf-8') as template_file:
     mac_template = template_file.read()
 
 # Replace NaN values in the DataFrame with empty strings
@@ -30,32 +30,38 @@ for index, row in df.iterrows():
     # Format the content by replacing placeholders with values from the DataFrame
     formatted_content = mac_template.format(
         Surname=row.get('Surname', ''),
+        Gender=row.get('Gender', ''),
+        Mortality=row.get('Mortality', ''),
+        DOB=row.get('DOB', ''),
+        Agent=formatted_agent,  # Add the formatted agent number here
+        Reff_Partner=formatted_Reff_Partner,  # Add the formatted agent number here
+        Mandate=row.get('Mandate', ''),
+        Referral=row.get('Referral', ''),
+        Branch=row.get('Branch', ''),
         Product=row.get('Product', ''),
         Payment_Method=row.get('Payment_Method', ''),
         Billing_Frequency=f"{row.get('Billing_Frequency', ''):02}",  # Ensures two digits
+        Height=row.get('Height', ''),
+        Weight=row.get('Weight', ''),
+        Insurable_Interest=row.get('Insurable_Interest', ''),
+        Cover_Type=row.get('Cover_Type', ''),
+        Risk_Cess_Age=row.get('Risk_Cess_Age', ''),
+        Premi_Term=row.get('Premi_Term', ''),
         Sum_Insured=row.get('Sum_Insured', ''),
-        Deductible=row.get('Deductible', ''),
-        Plan_Type=row.get('Plan_Type', ''),
         EPOL=row.get('EPOL', ''),
         ESUB=row.get('ESUB', ''),
         MAILFLAG=row.get('MAILFLAG', ''),
-        Gender=row.get('Gender', ''),
-        Until_Issue=row.get('Until_Issue', ''),
-        Mortality=row.get('Mortality', ''),
-        DOB=row.get('DOB', ''),
-        Waiver_Code=row.get('Waiver_Code', ''),
-        Risk_Cess_Age=row.get('Risk_Cess_Age', ''),
-        Premi_Term=row.get('Premi_Term', ''),
-        Agent=formatted_agent,  # Add the formatted agent number here
-        Reff_Partner=formatted_Reff_Partner,  # Add the formatted agent number here
-        Referral=row.get('Referral', ''),
+        Plan_Type=row.get('Plan_Type', ''),
+        Deductible=row.get('Deductible', ''),
+        Campaign_05D=row.get('Campaign_05D', ''),
+        Bank_Code=row.get('Bank_Code', ''),
         IBM_Path=ibm_path,  # Set IBM_Path to the full path of the .mac file
         TC_Next=index + 2,
         cell=index + 2
     )
 
     # Create the file path for the .mac file
-    mac_filename = f'l1j_{index + 1}.mac'  # The file name (e.g., bospo_1.mac)
+    mac_filename = f'PWMS_{index + 1}.mac'  # The file name (e.g., bospo_1.mac)
     mac_file_path = os.path.join(ibm_path, mac_filename)  # Combine IBM_PATH with the filename
 
     # Save the formatted content to the .mac file in the specified IBM_PATH
